68d071f7bb9c7fd6d41a8678fa0da27e8108f6f7
xiaolei <xuechao_lei@126.com>
2017-01-11 23:10:20 +0800

    注解

--- src/com/entity/Student.java
+++ src/com/entity/Student.java
@@ -3,22 +3,38 @@ package com.entity;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 //@Embeddable 表示一个非Entity类可以嵌入到另一个Entity类中作为属性而存在。
 @Entity
-@Table(name = "t_Students")
+@Table(name = "t_Student")
 public class Student {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	// @GeneratedValue(strategy = GenerationType.AUTO)
+	// @GeneratedValue(strategy = GenerationType.IDENTITY)
+	// @GeneratedValue(strategy = GenerationType.TABLE)
 	// id :必须，定义了映射到数据库表的主键属性，一个实体类有一个或者多个属性
 	// 被映射为主键，可置于主键属性或者getXX（）上。如果一个实体类定义了多个主键属性，该实体类必须实现serializable接口。
 	private Long sid;
-	@Id
 	private String name;
 	private String gender;
+	// @Temporal(TemporalType.DATE)
+	// @Temporal(TemporalType.TIME)
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date birthday;
 	private String major;
+	@Transient
+	private double salary;
+	// @ColumnDefault("Y")
+	// @Column(name = "isActive", columnDefinition = "String default Y")
+	private String isActive;
 	private Address add;
 
 	public Student() {
@@ -82,5 +98,21 @@ public class Student {
 
 	public void setAdd(Address add) {
 		this.add = add;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(String isActive) {
+		this.isActive = isActive;
 	}
 }
--- test/com/entity/TestStudent.java
+++ test/com/entity/TestStudent.java
@@ -9,7 +9,6 @@ import org.hibernate.service.ServiceRegistry;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 import org.hibernate.tool.schema.TargetType;
 import org.junit.Test;
-
 public class TestStudent {
 	@Test
 	public void testShemaExport() {
